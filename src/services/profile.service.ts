dea241e5c2c0c99d7f0625959d9bec09d552707a
suk-6 <me@suk.kr>
2024-02-24 13:32:51 +0900

    Add console logs to validateRegion function

--- src/services/profile.service.ts
+++ src/services/profile.service.ts
@@ -65,8 +65,11 @@ export class ProfileService {
 	}
 
 	async validateRegion(region: string) {
+		console.log("🚀 ~ ProfileService ~ validateRegion ~ region:", region);
 		const sido: string = region.split(" ")[0];
+		console.log("🚀 ~ ProfileService ~ validateRegion ~ sido:", sido);
 		const sigun: string = region.split(" ")[1];
+		console.log("🚀 ~ ProfileService ~ validateRegion ~ sigun:", sigun);
 
 		if (region.split(" ").length === 2)
 			if (Object.keys(regions).includes(sido))
